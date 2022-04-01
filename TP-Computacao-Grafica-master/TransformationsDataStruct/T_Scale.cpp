da34b5e5718f006aab2e288183c8d7161002e806
andrepinto42 <andrepinto.goncalves@gmail.com>
2022-04-01 21:45:31 +0100

    Phase 2

--- TP-Computacao-Grafica-master/TransformationsDataStruct/T_Scale.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by pinto on 31/03/22.
-//
-
-#include "T_Scale.h"
-
-
-T_Scale::T_Scale(float x,float y,float z):
-        x(x),y(y),z(z){}
--- TP-Computacao-Grafica-master/TransformationsDataStruct/Transform.h
+++ TP-Computacao-Grafica-master/TransformationsDataStruct/Transform.h
@@ -6,8 +6,39 @@
 #define CLASS1_TRANSFORM_H
 
 
+#include <iostream>
+
 class Transform {
+public:
+    Transform()= default;
+    virtual void Print()=0;
 };
 
+class T_Scale : public Transform{
+public:
+    float x,y,z;
+    T_Scale(float x,float y,float z):x(x),y(y),z(z){}
+    void Print() override{
+        std::cout << " I am Scale";
+    }
+};
+
+
+class T_Translate : public Transform{
+public:
+    float x,y,z;
+    T_Translate(float x,float y,float z):x(x),y(y),z(z){}
+    void Print() override{
+        std::cout << " I am Transform";
+    }
+};
+class T_Rotate : public Transform{
+public:
+    float angle,x,y,z;
+    T_Rotate(float angle,float x,float y,float z):angle(angle),x(x),y(y),z(z){}
+    void Print() override{
+        std::cout << " I am Rotate";
+    }
+};
 
 #endif //CLASS1_TRANSFORM_H
